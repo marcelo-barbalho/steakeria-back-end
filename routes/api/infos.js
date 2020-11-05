9e07fe27e1ba425c16ec7c4655abc4dc1134cdd0
Marcelo Barbalho <marcelo.barbalho@al.infnet.edu.br>
2020-11-05 00:44:51 -0300

    added put in infos

--- routes/api/infos.js
+++ routes/api/infos.js
@@ -9,6 +9,38 @@ const complete_link = require('../../service/complete_link')
 const get_max_order = require('../../service/get_max_order')
 
 
+// @route    PATCH /infos/:contentId-:infoId
+// @desc     UPDATE infos
+// @access   Private
+router.put('/:contentId-:infoId', auth, file, async (req, res, next) => {
+  try {
+    const contentId = req.params.contentId
+    const infoId = req.params.infoId
+    let content = await Content.findOne({_id : contentId})
+    if (req.body.photo_name) {
+      req.body.photo=`infos/${req.body.photo_name}`
+    }
+    if (content) {
+      for(let index in content.infos){
+        if (content.infos[index]._id == infoId) {
+          content.infos[index] = req.body
+        }
+      }
+      content = complete_link(content)
+      await content.save()
+      if (content.id) {
+        res.json(content)
+      }
+    } else {
+      res.status(404).send({ "error": MSGS.CONTENT404  })
+    }
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send({ "error": MSGS.GENERIC_ERROR})
+  }
+})
+
+
 // @route    POST /infos/:contentId
 // @desc     CREATE infos
 // @access   Private
